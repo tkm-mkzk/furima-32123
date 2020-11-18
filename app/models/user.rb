98b32de34386add17b8a93ae757b92cb68df8fb8
tkm_mkzk <t.m.08013440879@icloud.com>
2020-11-18 18:57:42 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,           presence: true
+  validates :first_name,         presence: true
+  validates :last_name,          presence: true
+  validates :first_name_reading, presence: true
+  validates :last_name_reading,  presence: true
+  validates :birthday,           presence: true
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
