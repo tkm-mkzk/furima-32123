18987cae88a6321b3900ef6684c61b3bc14e99aa
tkm_mkzk <t.m.08013440879@icloud.com>
2020-11-18 20:26:02 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,           presence: true
-  validates :first_name,         presence: true
-  validates :last_name,          presence: true
-  validates :first_name_reading, presence: true
-  validates :last_name_reading,  presence: true
+  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+  validates :first_name_reading, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :last_name_reading,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
   validates :birthday,           presence: true
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
 end
