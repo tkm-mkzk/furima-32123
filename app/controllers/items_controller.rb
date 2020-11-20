3e15f4c7e13066a8136c83d8e7fd48b2e8b081b8
tkm_mkzk <t.m.08013440879@icloud.com>
2020-11-20 19:15:35 +0900

    ストロングパラメーターとログイン状態に対する記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
   end
@@ -19,5 +20,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_description, :category_id, :item_condition_id, :shipping_cost_id, :shipping_source_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
