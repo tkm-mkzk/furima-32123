c73a515322188aabc43e3935bca01186edf66b39
tkm_mkzk <t.m.08013440879@icloud.com>
2020-11-20 11:59:01 +0900

    fakerを一意性のあるemaiだけに修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.name}
+    nickname              {"frima太郎"}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"aaa111"}
     password_confirmation {password}
     first_name            {"太郎"}
     last_name             {"山田"}
     first_name_reading    {"タロウ"}
-    first_name_reading    {"ヤマダ"}
-    bithday               {Faker::Date.backward}
+    last_name_reading     {"ヤマダ"}
+    birthday              {"19900101"}
   end
 end
