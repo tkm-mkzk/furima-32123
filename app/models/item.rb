51a26780b80e80e1bb4fbed80fe4bf5a2f4e68d6
tkm_mkzk <t.m.08013440879@icloud.com>
2020-11-21 17:51:10 +0900

    priceカラムへのバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :item_description
-    validates :price
+    validates :price, format: { with: /\A[0-9]+\z/, message: "is invalid. Input full-width characters."}
   end
 
   with_options numericality: { other_than: 1 }
@@ -18,4 +18,6 @@ class Item < ApplicationRecord
     validates :shipping_source
     validates :days_to_ship_id
   end
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 end
