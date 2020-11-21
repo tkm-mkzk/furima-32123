54885366252dcb77e59f7624d004c65a199c46ee
tkm_mkzk <t.m.08013440879@icloud.com>
2020-11-21 17:51:27 +0900

    アクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,24 +2,20 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-  end
-
-  def show
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
