51dc338b14fbb55b3a34ccc112bb1313a9258e61
tkm_mkzk <t.m.08013440879@icloud.com>
2020-11-21 15:40:51 +0900

    migrateの修正

--- db/migrate/20201120103217_create_shipping_sources.rb
+++
@@ -1,8 +0,0 @@
-class CreateShippingSources < ActiveRecord::Migration[6.0]
-  def change
-    create_table :shipping_sources do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20201120103241_create_days_to_ships.rb
+++
@@ -1,8 +0,0 @@
-class CreateDaysToShips < ActiveRecord::Migration[6.0]
-  def change
-    create_table :days_to_ships do |t|
-
-      t.timestamps
-    end
-  end
-end
